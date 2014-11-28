1efa9dd7aef97a291c2dc7f1f0d0d0ad4e5ba1fd
ZaruWright <samuel_gse@hotmail.com>
2014-11-28 19:09:16 +0100

    run, but error message in erase

---
+++ Agenda.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+## @Author: Samuel García Segador
+## @Copyright 2014 Samuel García Segador
+## @License GPL
+
+# CONSTANTS
+file=".diary" # Hide file
+fileError=".diaryErrors" # Hide file
+fileHit=".diaryHits" # we'll write in this file when the search is sucessfull
+prompt="#!"
+
+# Colors Code
+Black="30m"
+Red="31m"
+Green="32m"
+Yellow="33m"
+Blue="34m"
+Magenta="35m"
+Cyan="36m"
+Gray="37m"
+
+
+#
+# Simple menu to do the operations
+function menu(){
+	
+	let close=0 #number var
+	while [ $close -eq 0 ];
+	do
+		colorText $Yellow "1) "
+			colorText $Blue "Show\n"
+		colorText $Yellow "2) "
+			colorText $Blue "Search\n"
+		colorText $Yellow "3) "
+			colorText $Blue "Erase\n"
+		colorText $Yellow "4) "
+			colorText $Blue "Erase all\n"
+		colorText $Yellow "5) "
+			colorText $Blue "Add\n"
+		colorText $Yellow "6) "
+			colorText $Blue "Quit\n"
+		read -p $prompt option
+		case $option in
+			"1") show;;
+			"2") search;;
+			"3") erase;;
+			"4") eraseAll;;
+			"5") add;;
+			"6") colorText $Green "Goodbye!\n"; close=1;;
+			*)   colorText $Red "Please, select a correct answer.\n";;
+		esac
+	done
+}
+
+#
+# Shows to the user all the contacts that you have in it.
+function show(){
+
+	if [ -f $file ]; then
+		let lines=`wc $file | cut -d " " -f2` #number of lines
+		let i=1
+		while [ $i -le $lines ];
+		do
+			colorText $Green "Name: "; cat $file | cut -d ':' -f1 | tr '\n' ':' | cut -d ':' -f$i
+			colorText $Green "Phone: ";cat $file | cut -d ':' -f2 | tr '\n' ':' | cut -d ':' -f$i
+			colorText $Green "Mail: "; cat $file | cut -d ':' -f3 | tr '\n' ':' | cut -d ':' -f$i
+			colorText $Green "\n"
+			i=`expr $i + 1`
+		done
+	else
+		colorText $Red "There isn't nothing in the database, please add some contacts.\n"
+	fi
+}
+
+
+# 
+# The user can search in the database (file) the data of
+# your contacts
+function search(){
+
+	if [ -f $file ]; then
+		colorText $Green "Search: ";  read search
+		cat $file | grep "\<$search:.*:.*" > diaryAux 2> $fileError
+
+		if [ $? -eq 0 ];then 
+			# If the grep command finished correctly, then show the data
+			colorText $Green "Name: ";  cat diaryAux | cut -d ':' -f1 # It gets the first field
+			colorText $Green "Phone: "; cat diaryAux | cut -d ':' -f2 # It gets the second field
+			colorText $Green "Mail: ";  cat diaryAux | cut -d ':' -f3 # It gets the third field
+		else
+			colorText $Red "Sorry, this name isn't in your diary, try again.\n"
+		fi
+		rm diaryAux
+	else
+		colorText $Red "There isn't nothing in the database, please add some contacts.\n"
+	fi
+}
+
+#
+# Erase a contact from the database with an input string from the user
+function erase(){
+	if [ -f $file ]; then
+
+		colorText $Green "Name: ";  read name
+		let line=`cat $file | grep -n "\<$name:.*:.*" | cut -d ':' -f1`
+		if [ $? -eq 0 ]; then
+			sed ${line}d $file > diaryAux
+			cat diaryAux > $file
+			rm diaryAux
+			show
+		else
+			colorText $Red "Sorry, this name isn't in your diary, try again.\n"
+		fi
+	else
+
+		colorText $Red "There isn't nothing in the database, please add some contacts.\n"
+	fi
+	
+}
+
+#
+# Erase all about the diary
+function eraseAll(){
+	if [ -f $file ]; then
+		rm $file
+	else
+		colorText $Red "There isn't nothing in the database, please add some contacts.\n"
+	fi
+	touch $fileHit
+	touch $fileError
+}
+
+# 
+# Add a contact in the database
+function add(){
+
+	# We create file if not exist
+	if [ ! -f $file ]; then
+		touch $file
+	fi
+
+	colorText $Green "Name: ";  read name
+	cat $file | grep "\<$name:.*:.*" > $fileHit 
+	if [ $? -ne 0 ];then
+		colorText $Green "Phone: "; read phone
+		colorText $Green "Mail: " ; read mail
+		colorText $Green "\n"
+		echo "$name:$phone:$mail" >> $file
+	else
+		colorText $Red "Sorry, this name is in your diary yet, try again.\n"
+	fi
+	
+}
+
+function colorText(){
+	echo -e -n "\e[$1$2\e[0m"
+}
+
+# Start Program
+menu
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Diary-bash-Script
 =================
+
+A simple script with bash commands that shows the contacts that you have in it.
+
+This diary allows you add/remove/seach a contact in your diary.
+
+It has a few errors that if I have time I'll solve them.
